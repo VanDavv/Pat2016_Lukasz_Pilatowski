4f8c0ca20e8f67783a728ba871d28693772328fd
VanDavv <vandavv96@hotmail.com>
2016-01-31 08:57:50 +0100

    Updated Movie from my-app to map all the fields given by movieBundle

--- my-app/src/main/java/com/blstream/patronage/app/model/Movie.java
+++ my-app/src/main/java/com/blstream/patronage/app/model/Movie.java
@@ -1,12 +1,11 @@
 package com.blstream.patronage.app.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.net.URI;
 import java.util.List;
-import java.util.Objects;
 
 @Entity
 @Table(name = "Movies")
@@ -16,31 +15,72 @@ public class Movie {
 
     @JsonProperty
     @NotNull
-    private String movieName;
+    private String title;
     @JsonProperty
     private List<Actor> actors;
-    private com.blstream.patronage.movieDataBundle.Movie detailedMovieData;
+    private int year;
+    private String rated;
+    private String released;
+    private int runtime;
+    private String genres;
+    private String director;
+    private String writer;
+    private String detailedActors;
+    private String plot;
+    private String language;
+    private String country;
+    private String awards;
+    private URI poster;
+    private int metascore;
+    private float imdbRating;
+    private long imdbVotes;
+    private String imdbID;
+    private String type;
+    private boolean response;
 
     public Movie() {
 
     }
 
-    public Movie(String movieName, List<Actor> actors) {
-        this.movieName = movieName;
+    public Movie(String title, List<Actor> actors) {
+        this.title = title;
         this.actors = actors;
     }
 
     public void setId(long id) {
         this.id = id;
     }
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
+
+    public void setTitle(String title) {
+        this.title = title;
     }
     public void setActors(List<Actor> actors) {
         this.actors = actors;
     }
-    public void setDetailedMovieData(com.blstream.patronage.movieDataBundle.Movie detailedMovieData) {
-        this.detailedMovieData = detailedMovieData;
+
+    public Movie setDetailedMovieData(com.blstream.patronage.movieDataBundle.Movie detailedMovieData) {
+        if (detailedMovieData == null) return this;
+        this.setYear(detailedMovieData.getYear());
+        this.setRated(detailedMovieData.getRated());
+        this.setReleased(detailedMovieData.getReleased());
+        this.setRuntime(detailedMovieData.getRuntime());
+        this.setGenres(detailedMovieData.getGenres());
+        this.setDirector(detailedMovieData.getDirector());
+        this.setWriter(detailedMovieData.getWriter());
+        this.setDetailedActors(detailedMovieData.getActors());
+        this.setPlot(detailedMovieData.getPlot());
+        this.setLanguage(detailedMovieData.getLanguage());
+        this.setCountry(detailedMovieData.getCountry());
+        this.setAwards(detailedMovieData.getAwards());
+        this.setPoster(detailedMovieData.getPoster());
+        this.setMetascore(detailedMovieData.getMetascore());
+        this.setImdbRating(detailedMovieData.getImdbRating());
+        this.setImdbVotes(detailedMovieData.getImdbVotes());
+        this.setImdbID(detailedMovieData.getImdbID());
+        this.setType(detailedMovieData.getType());
+        this.setResponse(detailedMovieData.isResponse());
+
+        return this;
     }
 
     @Id
@@ -51,8 +91,8 @@ public class Movie {
     }
 
     @Column(name = "movieName", nullable = false)
-    public String getMovieName() {
-        return movieName;
+    public String getTitle() {
+        return title;
     }
 
     @OneToMany(cascade = CascadeType.ALL,mappedBy = "movie")
@@ -60,23 +100,262 @@ public class Movie {
         return actors;
     }
 
-    @Type(type = "serializable")
-    @Column(name = "movieDetailsObject")
-    public com.blstream.patronage.movieDataBundle.Movie getDetailedMovieData() {
-        return detailedMovieData;
+    @Column(name = "year")
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Column(name = "rated")
+    public String getRated() {
+        return rated;
+    }
+
+    public void setRated(String rated) {
+        this.rated = rated;
+    }
+
+    @Column(name = "released")
+    public String getReleased() {
+        return released;
+    }
+
+    public void setReleased(String released) {
+        this.released = released;
+    }
+
+    @Column(name = "runtime")
+    public int getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(int runtime) {
+        this.runtime = runtime;
+    }
+
+    @Column(name = "genres")
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    @Column(name = "director")
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    @Column(name = "writer")
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    @Column(name = "detailedActors")
+    public String getDetailedActors() {
+        return detailedActors;
+    }
+
+    public void setDetailedActors(String detailedActors) {
+        this.detailedActors = detailedActors;
+    }
+
+    @Column(name = "plot")
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    @Column(name = "language")
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    @Column(name = "country")
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Column(name = "awards")
+    public String getAwards() {
+        return awards;
+    }
+
+    public void setAwards(String awards) {
+        this.awards = awards;
+    }
+
+    @Column(name = "poster")
+    public URI getPoster() {
+        return poster;
+    }
+
+    public void setPoster(URI poster) {
+        this.poster = poster;
+    }
+
+    @Column(name = "metascore")
+    public int getMetascore() {
+        return metascore;
+    }
+
+    public void setMetascore(int metascore) {
+        this.metascore = metascore;
+    }
+
+    @Column(name = "imdbRating")
+    public float getImdbRating() {
+        return imdbRating;
+    }
+
+    public void setImdbRating(float imdbRating) {
+        this.imdbRating = imdbRating;
+    }
+
+    @Column(name = "imdbVotes")
+    public long getImdbVotes() {
+        return imdbVotes;
+    }
+
+    public void setImdbVotes(long imdbVotes) {
+        this.imdbVotes = imdbVotes;
+    }
+
+    @Column(name = "imdbID")
+    public String getImdbID() {
+        return imdbID;
+    }
+
+    public void setImdbID(String imdbID) {
+        this.imdbID = imdbID;
+    }
+
+    @Column(name = "type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Column(name = "response")
+    public boolean isResponse() {
+        return response;
+    }
+
+    public void setResponse(boolean response) {
+        this.response = response;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        if (id != movie.id) return false;
+        if (year != movie.year) return false;
+        if (runtime != movie.runtime) return false;
+        if (metascore != movie.metascore) return false;
+        if (Float.compare(movie.imdbRating, imdbRating) != 0) return false;
+        if (imdbVotes != movie.imdbVotes) return false;
+        if (response != movie.response) return false;
+        if (title != null ? !title.equals(movie.title) : movie.title != null) return false;
+        if (actors != null ? !actors.equals(movie.actors) : movie.actors != null) return false;
+        if (rated != null ? !rated.equals(movie.rated) : movie.rated != null) return false;
+        if (released != null ? !released.equals(movie.released) : movie.released != null) return false;
+        if (genres != null ? !genres.equals(movie.genres) : movie.genres != null) return false;
+        if (director != null ? !director.equals(movie.director) : movie.director != null) return false;
+        if (writer != null ? !writer.equals(movie.writer) : movie.writer != null) return false;
+        if (detailedActors != null ? !detailedActors.equals(movie.detailedActors) : movie.detailedActors != null)
+            return false;
+        if (plot != null ? !plot.equals(movie.plot) : movie.plot != null) return false;
+        if (language != null ? !language.equals(movie.language) : movie.language != null) return false;
+        if (country != null ? !country.equals(movie.country) : movie.country != null) return false;
+        if (awards != null ? !awards.equals(movie.awards) : movie.awards != null) return false;
+        if (poster != null ? !poster.equals(movie.poster) : movie.poster != null) return false;
+        if (imdbID != null ? !imdbID.equals(movie.imdbID) : movie.imdbID != null) return false;
+        return !(type != null ? !type.equals(movie.type) : movie.type != null);
+
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, movieName, actors);
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (actors != null ? actors.hashCode() : 0);
+        result = 31 * result + year;
+        result = 31 * result + (rated != null ? rated.hashCode() : 0);
+        result = 31 * result + (released != null ? released.hashCode() : 0);
+        result = 31 * result + runtime;
+        result = 31 * result + (genres != null ? genres.hashCode() : 0);
+        result = 31 * result + (director != null ? director.hashCode() : 0);
+        result = 31 * result + (writer != null ? writer.hashCode() : 0);
+        result = 31 * result + (detailedActors != null ? detailedActors.hashCode() : 0);
+        result = 31 * result + (plot != null ? plot.hashCode() : 0);
+        result = 31 * result + (language != null ? language.hashCode() : 0);
+        result = 31 * result + (country != null ? country.hashCode() : 0);
+        result = 31 * result + (awards != null ? awards.hashCode() : 0);
+        result = 31 * result + (poster != null ? poster.hashCode() : 0);
+        result = 31 * result + metascore;
+        result = 31 * result + (imdbRating != +0.0f ? Float.floatToIntBits(imdbRating) : 0);
+        result = 31 * result + (int) (imdbVotes ^ (imdbVotes >>> 32));
+        result = 31 * result + (imdbID != null ? imdbID.hashCode() : 0);
+        result = 31 * result + (type != null ? type.hashCode() : 0);
+        result = 31 * result + (response ? 1 : 0);
+        return result;
     }
 
     @Override
     public String toString() {
         return "Movie{" +
                 "id=" + id +
-                ", movieName='" + movieName + '\'' +
+                ", title='" + title + '\'' +
                 ", actors=" + actors +
+                ", year=" + year +
+                ", rated='" + rated + '\'' +
+                ", released='" + released + '\'' +
+                ", runtime=" + runtime +
+                ", genres='" + genres + '\'' +
+                ", director='" + director + '\'' +
+                ", writer='" + writer + '\'' +
+                ", detailedActors='" + detailedActors + '\'' +
+                ", plot='" + plot + '\'' +
+                ", language='" + language + '\'' +
+                ", country='" + country + '\'' +
+                ", awards='" + awards + '\'' +
+                ", poster=" + poster +
+                ", metascore=" + metascore +
+                ", imdbRating=" + imdbRating +
+                ", imdbVotes=" + imdbVotes +
+                ", imdbID='" + imdbID + '\'' +
+                ", type='" + type + '\'' +
+                ", response=" + response +
                 '}';
     }
 }
--- my-app/src/main/java/com/blstream/patronage/app/views/MovieView.java
+++ my-app/src/main/java/com/blstream/patronage/app/views/MovieView.java
@@ -3,26 +3,18 @@ package com.blstream.patronage.app.views;
 import com.blstream.patronage.app.model.Movie;
 import io.dropwizard.views.View;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 @SuppressWarnings("unused")
 public class MovieView extends View {
-    private final Set<Map.Entry<Movie, com.blstream.patronage.movieDataBundle.Movie>> movie;
+    private final List<Movie> movie;
 
     public MovieView(List<Movie> movies) {
         super("movie.mustache");
-
-        Map<Movie, com.blstream.patronage.movieDataBundle.Movie> mMap = new HashMap<>();
-        for (Movie m : movies) {
-            mMap.put(m, m.getDetailedMovieData());
-        }
-        movie = mMap.entrySet();
+        this.movie = movies;
     }
 
-    public Set<Map.Entry<Movie, com.blstream.patronage.movieDataBundle.Movie>> getMovie() {
+    public List<Movie> getMovie() {
         return movie;
     }
 }
